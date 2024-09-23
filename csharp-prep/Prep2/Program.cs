dc285aecef4d157b276ba5915e75f0989a796d47
owms-droid <mod20001@byui.edu>
2024-09-23 17:04:38 -0500

    Fix prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Write your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        string letter = "";
+
+        if (grade >= 90) 
+        {
+            letter = "A";
+        }
+        else if (grade >= 80) 
+        {
+            letter = "B";
+        }
+        else if (grade >= 70) 
+        {
+            letter = "C";
+        }
+        else if (grade >= 60) 
+        {
+            letter = "D";
+        }
+        else {
+            letter = "E";
+        }
+
+        Console.WriteLine($"You've got {letter}");
+
+        if (grade >= 70) {
+            Console.WriteLine("Congratulations, You passed the course!");
+        }
+        else {
+            Console.WriteLine("Sorry, but you didn't pass the course this time!");
+        }
     }
 }
