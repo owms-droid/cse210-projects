e57a5773d5bf0bd1040b683153e537e66caf4314
owms-droid <mod20001@byui.edu>
2024-09-24 00:31:49 -0500

    Fix prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.WriteLine("Tell me your guess: ");
+            string response2 = Console.ReadLine();
+            guess = int.Parse(response2);
+
+            if (guess == magic)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
